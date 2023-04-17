cb323219dcc9b79cb9c093217fac59166d97ee1b
Нисик <@DESKTOP-ALORV4C>
2023-04-17 14:03:13 +0300

    Добавьте файлы проекта.

---
+++ day 1 (3)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите двузначное число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int tens = num / 10; // находим десятки
+        int ones = num % 10; // находим единицы
+
+        Console.WriteLine($"Левая цифра: {tens}");
+        Console.WriteLine($"Правая цифра: {ones}");
+    }
+}
